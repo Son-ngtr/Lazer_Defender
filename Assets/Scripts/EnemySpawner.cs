8fe1eed758dae98428b3b061e7eeda2c1ea7b21c
Son Nguyen Xuan <Son.Nguyen.20022020@gmail.com>
2024-11-21 14:19:11 +0700

    Update

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -14,7 +14,7 @@ public class EnemySpawner : MonoBehaviour
     {
         StartCoroutine(SpawnEnemyWaves());
     }
-
+        
     public WaveConfigSO GetCurrentWave()
     {
         return currentWave;
@@ -43,3 +43,82 @@ public class EnemySpawner : MonoBehaviour
         } while (isLooping);
     }
 }
+
+
+
+/*using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping = true;
+    WaveConfigSO currentWave;
+
+    // Dictionary to hold pools for each enemy type
+    private Dictionary<GameObject, Queue<GameObject>> enemyPools;
+
+    private void Start()
+    {
+        enemyPools = new Dictionary<GameObject, Queue<GameObject>>();
+        StartCoroutine(SpawnEnemyWaves());
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+
+    // Method to get an enemy from the pool
+    private GameObject GetEnemyFromPool(GameObject enemyPrefab)
+    {
+        if (!enemyPools.ContainsKey(enemyPrefab))
+        {
+            enemyPools[enemyPrefab] = new Queue<GameObject>();
+        }
+
+        if (enemyPools[enemyPrefab].Count == 0)
+        {
+            // Instantiate a new enemy if the pool is empty
+            GameObject newEnemy = Instantiate(enemyPrefab);
+            newEnemy.SetActive(false);
+            enemyPools[enemyPrefab].Enqueue(newEnemy);
+        }
+
+        GameObject enemy = enemyPools[enemyPrefab].Dequeue();
+        enemy.SetActive(true);
+        return enemy;
+    }
+
+    // Method to return an enemy to the pool
+    public void ReturnEnemyToPool(GameObject enemy, GameObject enemyPrefab)
+    {
+        enemy.SetActive(false);
+        enemyPools[enemyPrefab].Enqueue(enemy);
+    }
+
+    // Using coroutine to Spawn enemies at different time
+    IEnumerator SpawnEnemyWaves()
+    {
+        do
+        {
+            foreach (WaveConfigSO wave in waveConfigs)
+            {
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    GameObject enemyPrefab = currentWave.GetEnemyPrefabs(i);
+                    GameObject enemy = GetEnemyFromPool(enemyPrefab);
+                    enemy.transform.position = currentWave.GetStartingWavepoint().position;
+                    enemy.transform.rotation = Quaternion.Euler(0, 0, 180);
+                    enemy.transform.parent = transform;
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
+            }
+        } while (isLooping);
+    }
+}*/
